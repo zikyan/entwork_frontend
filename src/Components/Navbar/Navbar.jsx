d7ade830d13f07e58f69d1e3d3a58ef8cbf84f55
zikyan <zikyanrasheed786@gmail.com>
2021-12-27 22:48:13 +0500

    setup initial things

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <div className='navbar-parent'>
+            <h1>this is navbar</h1>
+        </div>
+    )
+}
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.css';
+
+export default function Home() {
+    return (
+        <div className='home-parent'>
+            <h1>Summer is not wasted. This is fun</h1>
+            <p>hello from the other side</p>
+        </div>
+    )
+}
