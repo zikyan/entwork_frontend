b7461b0aa278734e75b004464e36f6ad6b62cc4e
zadar2000 <abdullahzafar2077@gmail.com>
2022-02-24 07:48:37 +0500

    FinalFrontEnd

--- src/Pages/TwitterVirals/TwitterVirals.jsx
+++ src/Pages/TwitterVirals/TwitterVirals.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import "./twittervirals.css"
 export default function TwitterVirals() {
   return (
-    <div>TwitterVirals</div>
+    <div className="twitterPosts">
+      <div className='posts'>
+      <TwitterTweetEmbed
+  tweetId={'933354946111705097'}
+/>
+ <TwitterTweetEmbed
+ tweetId='1494763670974480395'
+ />
+
+<TwitterTweetEmbed
+ tweetId='1270093417922560002'
+ />
+ </div>
+    </div>
   )
 }
